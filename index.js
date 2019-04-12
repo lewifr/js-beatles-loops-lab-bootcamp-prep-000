b20bbacab64c7e43d8c0b4dc391882e9ea8c0a71
Francis Lewis <francis.lewis@me.com>
2019-04-12 14:04:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesPlay(musicans, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   let result = []
   for (let counter = 0; counter < musicians.length; counter++) {
     result.push(`${musicians[counter]} plays ${instruments[counter]}}`)
