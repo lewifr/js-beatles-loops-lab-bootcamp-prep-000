38a2fff84824512128f8cfe7cb38f7cccef70551
Francis Lewis <francis.lewis@me.com>
2019-04-12 14:02:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicans, instruments) {
+  let result = []
+  for (let counter = 0; counter < musicians.length; counter++) {
+    result.push(`${musicians[counter] plays ${instruments[counter]}}`)
+  }
+}
